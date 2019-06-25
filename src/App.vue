59c8ff6d6d149f5311555bd6deb0000a68a6f9df
donunobunjo <don.unobunjo@gmail.com>
2019-06-25 11:48:57 +0100

    First init, modified the App.vue component

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+     <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+          <div id="logo" class="fl_left">
+        <h1><a href="#">logistikPRO</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <!--<li class="active"><a href="index.html">Home</a></li>-->
+          <li><a href="#">Home</a></li>
+          <li><a class="drop" href="#">Register</a>
+            <ul>
+              <li><a href="#">Client</a></li>
+              <li><a href="#">Product</a></li>
+              
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Transaction</a>
+            <ul>
+              <li><a href="#">Reciept</a></li>
+              <li><a href="#">Issuance</a></li>
+              <li><a class="drop" href="#">Returns</a>
+                <ul>
+                  <li><a href="#">Inwards</a></li>
+                  <li><a href="#">Outwards</a></li>
+                </ul>
+              </li>
+              
+            </ul>
+          </li>
+          <li><a class="drop" href="#">History</a>
+            <ul>
+              <li><a href="#">Client</a></li>
+              <li><a href="">Product</a></li>
+              
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      
+    </header>
+  </div>
+    
+  
+    
+  <div class="wrapper row3">
+    <main class="hoc container clear"> 
+    
+    </main>
+  </div>
+  <div class="wrapper row5">
+    <div id="copyright" class="hoc clear"> 
+      <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">LogistikPRO</a></p>
+      
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+ 
+}
+</script>
+
+<style>
+
+</style>

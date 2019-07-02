4380d2c28b43c4f93ceefd63314d4b5712b81e36
donunobunjo <don.unobunjo@gmail.com>
2019-07-02 13:49:11 +0100

    implemented the 'create' part for product

--- src/components/CreateProduct.vue
+++ src/components/CreateProduct.vue
@@ -6,7 +6,7 @@
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="productid">Product ID <span>*</span></label>
-      <input type="text" class="form-control" id="productid" placeholder="Product ID" name="productid" v-model="productid" v-validate="'required|alpha'">
+      <input v-model="product.productid" type="text" class="form-control" id="productid" placeholder="Product ID" name="productid" v-validate="'required'">
       <span v-if="errors.has('productid')" class="errorms">
         {{ errors.first('productid') }}
       </span>
@@ -15,7 +15,7 @@
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="productname">Product Name<span>*</span></label>
-      <input type="text" class="form-control" id="productname" placeholder="product Name" name="productname" v-model="productname" v-validate="'required|alpha'">
+      <input v-model="product.productname" type="text" class="form-control" id="productname" placeholder="product Name" name="productname" v-validate="'required|alpha'">
       <span v-if="errors.has('productname')" class="errorms">
         {{ errors.first('productname') }}
       </span>
@@ -26,7 +26,7 @@
   <div class="form-row">
     <div class="form-group col-md-4">
       <label for="reorderlevel">Re-Order Level<span>*</span></label>
-      <input type="number" class="form-control" id="reorderlevel" placeholder="Re-order level" name="reorderlevel" v-model="reorderlevel" v-validate="'required|digits:2'">
+      <input v-model="product.reorderlevel" type="number" class="form-control" id="reorderlevel" placeholder="Re-order level" name="reorderlevel" v-validate="'required|numeric'">
       <span v-if="errors.has('reorderlevel')" class="errorms">
         {{ errors.first('reorderlevel') }}
       </span>
@@ -35,39 +35,83 @@
   <div class="form-row">  
     <div class="form-group col-md-2">
       <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck" checked >
+        <input v-model="product.active" class="form-check-input" type="checkbox" id="gridCheck" >
         <label class="form-check-label" for="gridCheck">
         Active
         </label>
       </div>
     </div>
   </div>
-  
-  <button type="submit" class="btn btn-primary">Save</button>
+   <button type="submit" class="btn btn-primary">Save</button>
   
 </form>
+  <div class="row">
+          <table class="table table-hover">
+            <thead>
+            <tr>
+              <th>ProductID</th>
+              <th>Product Name</th>
+              <th>Re-order Level</th>
+              <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product._id">
+                  <td>{{ product.productid }}</td>
+                  <td>{{ product.productname }}</td>
+                  <td>{{ product.reorderlevel }}</td>
+                  <td><router-link :to="{name: 'edit', params: { id: product._id }}" class="btn btn-primary">Edit</router-link></td>
+                  <td><button class="btn btn-danger" @click.prevent="deleteProduct(product._id)">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
+  </div>
+  
+
     
   </div>
 </template>
 
 <script>
 export default {
-     methods: {
-        createProduct() {
-          this.$validator.validateAll().then(res => {
-            if (res) {
-              alert('Form successfully submitted!')
-            } else {
-              alert('Please correct all error!')
-            }
-          })
+  data() {
+    return {
+      product: {
+        productid: "",
+        productname: "",
+        reorderlevel: "",
+        active: true
+      },
+      products: []
+    };
+  },
+  methods: {
+    createProduct() {
+      this.$validator.validateAll().then(res => {
+        if (res) {
+          //alert('Form successfully submitted!')
+          let uri = "http://localhost:4000/products/add";
+          this.axios.post(uri, this.product).then(response => {
+            //this.$router.push({name: 'product'});
+            //this.product={};
+            // console.log(response.data._id);
+            //console.log(response.data.productid);
+            //console.log(response.data.productname);
+            console.log("successful saved");
+          });
+
+          // console.log("successful saved")
+        } else {
+          //alert('Please correct all error!')
+          console.log("Error occured");
         }
-     }
+      });
+    }
+  }
 };
 </script>
 <style>
-    .errorms{
-      color: red;
-      
-    }
+.errorms {
+  color: red;
+}
 </style>

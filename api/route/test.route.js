2255f397d38e19d7304bd6e219ee66e362a6ba9b
donunobunjo <don.unobunjo@gmail.com>
2019-11-08 09:25:33 +0100

    Before deploy to heroku

--- api/route/test.route.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const testRouter = express.Router();
-const testController = require('../controller/test.controller');
-testRouter.get('/getbalances', testController.getBalances);
-
-module.exports = testRouter;

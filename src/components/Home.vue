e6e426a2392ae36c27a6e5cccabb2291e3c4b1bc
donunobunjo <don.unobunjo@gmail.com>
2019-11-08 14:44:21 +0100

    modified home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,22 @@
 <template>
+<div>
   <div class="row justify-content-center" id="overlay">
-      <h1>Home</h1>
+      <h1>LogistiksPro</h1>
       <hr>
-  </div>
+      <p>A warehouse inventory/logistics application develop by Don Unobunjo for demonstration purpose only</p>
+      <p>The application was created using the following stack:</p><br>
+      <p>
+      <ol>
+        <li>Mongodb</li>
+        <li>ExpressJS</li>
+        <li>VueJS</li>
+        <li>Node</li>
+      </ol>
+      </p>
+      
+   </div>
+   <h3>Functionalities</h3>
+</div>
 </template>
 <script>
 export default {
